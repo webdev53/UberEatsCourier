2530f90dc036e52d7a1d54a1f30c426a3ce829e5
Web Dev <web.2019.dev@gmail.com>
2022-04-21 13:10:28 +0545

    order item screen added

--- App.js
+++ App.js
@@ -1,36 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import orders from './assets/data/orders.json';
-import { Entypo } from '@expo/vector-icons';
+import OrderItem from './src/components/OrderListItem';
 
 const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: 'row',
-          borderColor: '#3FC060',
-          borderWidth: 2,
-          borderRadius: 10,
-        }}
-      >
-        <Image
-          source={{ uri: order.Restaurant.image }}
-          style={{ width: 50, height: 50 }}
-        />
+      <FlatList
+        data={orders}
+        renderItem={({ item }) => <OrderItem order={item} />}
+      />
 
-        <View>
-          <Text>{order.Restaurant.name}</Text>
-          <Text>{order.Restaurant.address}</Text>
-          <Text>Delivery Details:</Text>
-
-          <Text>{order.User.name}</Text>
-          <Text>{order.User.address}</Text>
-        </View>
-        <Entypo name="check" size={30} color="black" />
-      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -40,7 +22,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50,
   },
 });
---
+++ src/components/OrderListItem/index.js
@@ -0,0 +1,71 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import orders from '../../../assets/data/orders.json';
+import { Entypo } from '@expo/vector-icons';
+
+const OrderItem = ({ order }) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        borderColor: '#3FC060',
+        borderWidth: 2,
+        borderRadius: 12,
+        margin: 20,
+      }}
+    >
+      <Image
+        source={{ uri: order.Restaurant.image }}
+        style={{
+          width: '25%',
+          height: '100%',
+          borderBottomLeftRadius: 10,
+          borderTopLeftRadius: 10,
+          borderRadius: 10,
+        }}
+      />
+
+      <View style={{ flex: 1, marginLeft: 10, paddingVertical: 5 }}>
+        <Text style={{ fontSize: 18, fontWeight: '500' }}>
+          {order.Restaurant.name}
+        </Text>
+        <Text style={{ color: 'grey' }}>{order.Restaurant.address}</Text>
+
+        <Text style={{ marginTop: 10 }}>Delivery Details:</Text>
+
+        <Text style={{ color: 'grey' }}>{order.User.name}</Text>
+        <Text style={{ color: 'grey' }}>{order.User.address}</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: '#3FC060',
+          borderBottomRightRadius: 10,
+          borderTopRightRadius: 10,
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 5,
+        }}
+      >
+        <Entypo
+          name="check"
+          size={30}
+          color="white"
+          style={{
+            marginLeft: 'auto',
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default OrderItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
