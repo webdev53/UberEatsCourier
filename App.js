c17527d46489a4f04635603c37a3bd2e52230d75
Web Dev <web.2019.dev@gmail.com>
2022-04-21 08:15:30 +0545

    order layout added

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import orders from './assets/data/orders.json';
+import { Entypo } from '@expo/vector-icons';
+
+const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          borderColor: '#3FC060',
+          borderWidth: 2,
+          borderRadius: 10,
+        }}
+      >
+        <Image
+          source={{ uri: order.Restaurant.image }}
+          style={{ width: 50, height: 50 }}
+        />
+
+        <View>
+          <Text>{order.Restaurant.name}</Text>
+          <Text>{order.Restaurant.address}</Text>
+          <Text>Delivery Details:</Text>
+
+          <Text>{order.User.name}</Text>
+          <Text>{order.User.address}</Text>
+        </View>
+        <Entypo name="check" size={30} color="black" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
